578f45ef57d595e45f0dc890b3e0daecae1fa601
afrochic <52344898+afrochic@users.noreply.github.com>
2022-02-06 15:57:20 +0300

    Add files via upload
    IoT 
    jan24i is for the wifi module
    jan24j is for the sensor module

---
+++ sketch_jan24i/sketch_jan24i.ino
@@ -0,0 +1,67 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+#define FIREBASE_HOST "vaulted-scholar-284900-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "HG8eu2PLzh0ZpfW3S0jaMLqLqxuF71DulTvg1d7W"
+#define WIFI_SSID "iPhone 7 Plus"
+#define WIFI_PASSWORD "ndinda1999"
+
+
+String values,sensor_data;
+
+void setup() {
+  //Initializes the serial connection at 9600 get sensor data from arduino.
+   Serial.begin(9600);
+   
+  delay(1000);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    
+  }
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
+  
+}
+void loop() {
+
+ bool Sr =false;
+ 
+  while(Serial.available()){
+    
+        //get sensor data from serial put in sensor_data
+        sensor_data=Serial.readString(); 
+        Sr=true;    
+        
+    }
+  
+delay(1000);
+
+  if(Sr==true){  
+    
+  values=sensor_data;
+  
+  //get comma indexes from values variable
+  int fristCommaIndex = values.indexOf(',');
+ 
+  
+  //get sensors data from values variable by  spliting by commas and put in to variables  
+  String ultrasonic_value = values.substring(0, fristCommaIndex);
+ 
+  //store ultrasonic sensor data as string in firebase 
+  Firebase.setString("ultrasonic_value",ultrasonic_value);
+   delay(10);
+  
+  //store previous sensors data as string in firebase
+  
+  Firebase.pushString("previous_ultrasonic_value",ultrasonic_value);
+ 
+  delay(1000);
+  
+  if (Firebase.failed()) {  
+      return;
+  }
+  
+}   
+}
---
+++ sketch_jan24j/sketch_jan24j.ino
@@ -0,0 +1,52 @@
+int trigger_pin = 2;
+int echo_pin = 3;
+ 
+
+int time;
+int distance; 
+String values;
+
+ void setup() { 
+  
+ pinMode (trigger_pin, OUTPUT); 
+ 
+ pinMode (echo_pin, INPUT);
+ 
+ //Initializes the serial connection at 9600 to sent sensor data to ESP8266.
+ Serial.begin(9600); 
+ 
+delay(2000);  
+  
+ }
+
+ 
+void loop() {
+
+  // get sensors data and put in to values variables as a string.
+   values= (get_distance_Value()+',');
+       delay(1000);
+       // removed any buffered previous serial data.
+       Serial.flush();
+       delay(1000);
+       // sent sensors data to serial (sent sensors data to ESP8266)
+       Serial.print(values);
+       delay(2000);
+ 
+
+}
+
+// get Ultrasonic sensor data
+String get_distance_Value(){  
+ 
+ 
+    digitalWrite (trigger_pin, HIGH);
+
+    delayMicroseconds (10);
+
+    digitalWrite (trigger_pin, LOW);
+
+    time = pulseIn (echo_pin, HIGH);
+
+    distance = (time * 0.034) / 2;
+      return String(distance);  
+}
